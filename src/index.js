3752bd89dda525701442e01bb86bc089650892aa
NP Home <nicolas.physentzides@gmail.com>
2025-02-27 00:02:48 +0200

    27/02/2025 12:02 commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import StarRating from "./StarRating";
+import PropTypes from "prop-types";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+StarRating.propTypes = {
+  maxRating: PropTypes.number.isRequired,
+  message: PropTypes.array,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  className: PropTypes.string,
+  defaultRating: PropTypes.number,
+  onSetRating: PropTypes.func,
+};
+
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating color="green" maxRating={10} onSetRating={setMovieRating} />
+//       <p>Rating is {movieRating}</p>
+//     </div>
+//   );
+// }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <StarRating
+      maxRating={5}
+      message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={48} color="red" className="test" defaultRating={3} /> */}
+    {/* <Test /> */}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
